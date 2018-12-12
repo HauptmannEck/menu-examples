f8deb909f0b9d33649591e34874d695cac6f66bc
Josh Eckhoff <jeckhoff@conserviscorp.com>
2018-12-12 16:21:49 -0600

    Updates

---
+++ precache-manifest.547b8d59b29bf044051ce66111f9efc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "437eebf7065dffa9028b04fd7cfb2cca",
+    "url": "/menu-examples/static/media/image2.437eebf7.png"
+  },
+  {
+    "revision": "2f204b561f01290c1f0186f4055e9e95",
+    "url": "/menu-examples/static/media/image.2f204b56.png"
+  },
+  {
+    "revision": "6d84b825c593914322a6",
+    "url": "/menu-examples/static/js/runtime~main.6d84b825.js"
+  },
+  {
+    "revision": "6370207484a674344117",
+    "url": "/menu-examples/static/js/main.63702074.chunk.js"
+  },
+  {
+    "revision": "00b30f008ac2c6b29a99",
+    "url": "/menu-examples/static/js/1.00b30f00.chunk.js"
+  },
+  {
+    "revision": "6370207484a674344117",
+    "url": "/menu-examples/static/css/main.bacf6626.chunk.css"
+  },
+  {
+    "revision": "00b30f008ac2c6b29a99",
+    "url": "/menu-examples/static/css/1.5a8279fa.chunk.css"
+  },
+  {
+    "revision": "541a63d69353b17a7931493a448f5ac9",
+    "url": "/menu-examples/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.baae5c87e127c2a73aeb4fb17557313e.js"
+  "/menu-examples/precache-manifest.547b8d59b29bf044051ce66111f9efc9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/menu-examples/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
